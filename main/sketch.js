300495824e0e9ca61d6ff52cfce1db48a4483091
Zachary Perrico <77283237+ZacharyPerrico@users.noreply.github.com>
2021-11-25 13:08:51 -0500

    Add files via upload

---
+++ main/sketch.js
@@ -0,0 +1,34 @@
+/*------------------------------*\
+ *                              *
+ *  Java Script Object Tracker  *
+ *        Version 2.4.2         *
+ *                              *
+ *       Zachary Perrico        *
+ *                              *
+\*------------------------------*/
+
+ruler = {
+  x1: 55, 
+  y1: 10,
+  x2: 125, 
+  y2: 10,
+};
+
+bounds = {
+  xMin: 50,
+  yMin: 29,
+  xMax: 208,
+  yMax: 214,
+};
+
+fileName = GIF_THROW;
+
+fps = 240; // Usually 240
+
+objectColors = [
+  [169, 40, 51],
+];
+
+backgroundColors = [
+  [81, 69, 69],
+];
